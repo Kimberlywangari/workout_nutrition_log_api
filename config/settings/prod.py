b4c3edcf7642dab54d7634a9d78a59a6aa54f9ca
Kimberly Wangari <kimberlywangarike@gmail.com>
2026-09-08 09:33:12 +0300

    Add production Docker configuration

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -6,4 +6,9 @@ DATABASES = {
     'default': dj_database_url.config()
 }
 DEBUG = False
-ALLOWED_HOSTS = ['your-production-domain.com']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='206.189.235.97').split(',')
+CSRF_TRUSTED_ORIGINS = config(
+    'CSRF_TRUSTED_ORIGINS',
+    default='http://206.189.235.97',
+).split(',')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
